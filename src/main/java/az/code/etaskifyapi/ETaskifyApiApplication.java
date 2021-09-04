32baf1b600e1b48f60341b21a7e41f57ea36fe77
VusalRA <vusalra@code.edu.az>
2021-09-05 00:27:24 +0400

    main: Initial commit

---
+++ src/main/java/az/code/etaskifyapi/ETaskifyApiApplication.java
@@ -0,0 +1,13 @@
+package az.code.etaskifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETaskifyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ETaskifyApiApplication.class, args);
+    }
+
+}
